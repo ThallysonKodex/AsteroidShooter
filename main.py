40822e68568e481b941b5ab8eded3dd1b6bdeb5d
ThallysonKodex <thallysontrader@gmail.com>
2022-10-05 01:41:13 -0400

    The final project!

--- main.py
+++ main.py
@@ -131,3 +131,4 @@ while True:
     pygame.display.update()
 
 
+# By ThallysonKodex
---
+++ settings.py
@@ -0,0 +1,13 @@
+level = ['                    ',
+         '                    ',
+         '                    ',
+         '                    ',
+         '                    ',
+         '                    ',
+         '                    ',
+         '                    ',
+         '                    ',
+         '  XX  XX  XX        ',
+         'XXXXXXXXXXXXXXXXXXXX']
+
+tile_size = 64
